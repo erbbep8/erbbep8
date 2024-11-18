489ad7f9d5fae65baaa320a3d666fdec50d0cf45
erbbep8 <erbbep8@gmail.com>
2024-11-18 17:42:57 +0800

    Add Feature Upload File V1

--- final-proj/app.js
+++ final-proj/app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+const fileUpload = require('./routes/fileUpload')
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/fileUpload', fileUpload)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
